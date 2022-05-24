89b9e45d669de2b69ce32c531ee022c94a5c811c
k-e-r <damajonboua@gmail.com>
2022-05-23 23:47:02 -0700

    add header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.scss';
+import { Menu2 } from 'tabler-icons-react';
+import { Settings } from 'tabler-icons-react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div className='header__left-items'>
+        <button>
+          <Menu2 size={25} strokeWidth={1.2} className='header__menubar' />
+        </button>
+      </div>
+      <div className='header__right-items'>
+        <button>
+          <Settings size={25} strokeWidth={1.2} className='header__settings' />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
